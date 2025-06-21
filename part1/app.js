e2d556a9a9b9c9295e9a93df6342743147c56a02
Shawn <xhzeng7091@gmail.com>
2025-06-21 06:01:05 +0000

    [automatic commit] 2025-06-21 06:01:05 UTC

--- part1/app.js
+++ part1/app.js
@@ -20,6 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/')
+app.use('/api/dogs',dogRouter);
 
 module.exports = app;
