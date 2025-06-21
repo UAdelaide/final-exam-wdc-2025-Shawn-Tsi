10bd6e9504c87366b1972a3e15da7674812e77fc
Shawn <xhzeng7091@gmail.com>
2025-06-21 06:21:12 +0000

    [automatic commit] 2025-06-21 06:21:11 UTC

--- part1/models/db.js
+++ part1/models/db.js
@@ -3,9 +3,10 @@ const mysql=require('mysql2/promise');
 const pool = mysql.createPool({
   host:     'localhost',
   user:     'root',
-  password: '',            // your MySQL root password (if any)
-  database: 'DogWalkService',
+  password: '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+module.exports=pool;
