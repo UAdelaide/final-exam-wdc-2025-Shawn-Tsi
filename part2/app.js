700dd497d52998364b94d94c22cc18f1f11ca1a3
Shawn <xhzeng7091@gmail.com>
2025-06-21 07:16:43 +0000

    [automatic commit] 2025-06-21 07:16:43 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
+const session = require('express-session');
 
 const app = express();
 
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(session({
+  secret: 'your‐secret‐here',
+  resave: false,
+  saveUninitialized: false
+}));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
